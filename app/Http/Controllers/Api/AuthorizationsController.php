d1027f7ab5062f953d5dad1d2c8f976fef521ee1
vagrant <vagrant@homestead>
2021-02-09 04:18:24 +0000

    快速生成 jwt

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -5,37 +5,57 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
-use Overtrue\Socialite\AccessToken;
 use Illuminate\Auth\AuthenticationException;
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 
 class AuthorizationsController extends Controller
 {
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            throw new AuthenticationException('用户名或密码错误');
+        }
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
-        $driver = \Socialite::driver($type);
+        $driver = \Socialite::create($type);
 
         try {
             if ($code = $request->code) {
-                $accessToken = $driver->getAccessToken($code);
+                $oauthUser = $driver->userFromCode($code);
             } else {
                 $tokenData['access_token'] = $request->access_token;
 
                 // 微信需要增加 openid
                 if ($type == 'wechat') {
-                    $tokenData['openid'] = $request->openid;
+                    $driver->withOpenid($request->openid);
                 }
-                $accessToken = new AccessToken($tokenData);
-            }
 
-            $oauthUser = $driver->user($accessToken);
+                $oauthUser = $driver->userFromToken($request->access_token);
+            }
         } catch (\Exception $e) {
+           throw new AuthenticationException('参数错误，未获取用户信息');
+        }
+
+        if (!$oauthUser->getId()) {
            throw new AuthenticationException('参数错误，未获取用户信息');
         }
 
         switch ($type) {
             case 'wechat':
-                $unionid = $oauthUser->getOriginal()['unionid'] ?? null;
+                $unionid = $oauthUser->getRaw()['unionid'] ?? null;
 
                 if ($unionid) {
                     $user = User::where('weixin_unionid', $unionid)->first();
@@ -56,6 +76,27 @@ class AuthorizationsController extends Controller
                 break;
         }
 
-        return response()->json(['token' => $user->id]);
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function update()
+    {
+        $token = auth('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        auth('api')->logout();
+        return response(null, 204);
     }
 }
